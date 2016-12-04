30dd737953418cc51ed6d4691dd28b4cf3beca01
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-04 16:34:01 -0600

    Customers now able to select item and quantity and complete a purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,6 +30,79 @@ function displayItems() {
 		}
 		//display table
 		console.log(itemsTable.toString());
+		customer();
 	})
 }
 
+function customer() {
+	inquirer.prompt([
+		{
+			name: "productId",
+			message: "Enter the ID of the product you would like to buy.",
+			type: "input",
+			validate: function(value) {
+		      if (isNaN(value) === false) {
+		        return true;
+		      }
+		      return false;
+		    }
+		},
+		{
+			name: "quantity",
+			message: "How many would you like to buy?",
+			type: "input",
+			validate: function(value) {
+      		if (isNaN(value) === false) {
+		        return true;
+		      }
+		      return false;
+		    }
+		}
+
+	]).then(function(input) {
+
+		connection.query("SELECT * FROM products WHERE ?", {item_id: input.productId}, function(error, response) {
+			if (error) throw error;
+
+			if (parseInt(input.quantity) <= response[0].stock_quantity) {
+				var orderCost = (parseInt(input.quantity) * response[0].price).toFixed(2);
+				console.log("Your total is $ " + orderCost);
+				fulfillOrder(response[0].item_id, response[0].stock_quantity, parseInt(input.quantity));
+			} else {
+				console.log("Sorry, we only have " + response[0].stock_quantity + " units left. We can't fulfill your order.");
+				keepShopping();
+			}
+		})
+	});
+}
+
+function fulfillOrder(itemId, currentQuantity, quantityPurchased) {
+	connection.query("UPDATE products SET ? WHERE ?", [{
+		stock_quantity: currentQuantity - quantityPurchased
+	}, {
+		item_id: itemId
+	}], function(error, response) {
+		if (error) throw error;
+
+		console.log("Order complete. Thank you for shoppign with Bamazon.");
+		keepShopping();
+	})
+}
+
+function keepShopping() {
+	inquirer.prompt({
+		name: "keepShopping",
+		message: "Would you like to make another purchase?",
+		type: "list",
+		choices: ["Yes", "No"]
+
+	}).then(function(input) {
+
+		if (input.keepShopping === "Yes") {
+			displayItems();
+		} else {
+			console.log("Come back soon!");
+			connection.end();
+		}
+	})
+}
